66bdd4521f85c971c235aea79c076534bbf5a461
Avdhesh Pal <87435192+AvdheshPal@users.noreply.github.com>
2026-06-21 03:29:01 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+       const cleaned = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        console.log(cleaned)
+        let i = 0;
+        let j = cleaned.length-1;
+        while(i < j){
+            if(cleaned[i] !== cleaned[j]) return false;
+            i++
+            j--
+        }
+        return true;
+    }
+}
